b151aaf62bce37b757fd77490f8efa1ef3fc68c7
Joseph Ko <koseph123@gmail.com>
2025-02-19 14:36:56 -0500

    filename bug

--- scripts/01-scrape-download.py
+++ scripts/01-scrape-download.py
@@ -14,9 +14,10 @@ save_directory = '/home/jko/spherical-code/data/text-files'
 # find all txt links and save
 soup = BeautifulSoup(response.content, "html.parser")
 links = soup.find_all('a', href=True)
-for link in links:squeue
+for link in links:
     if '.txt' in link['href']:
         filename = link['href']
+        filename = filename.strip()
         save_path = os.path.join(save_directory, filename)
         file_url = urljoin(page_url, filename)
         text_response = requests.get(file_url)
